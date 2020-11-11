12dcc18dd7703f235092c410bc6025c7155c5af8
Daniil <fspiritlan@gmail.com>
2020-11-11 20:51:10 +0300

    Создание основного метода для работы алгоритма

--- Algoritmic/DiagonalSnake.cs
+++ Algoritmic/DiagonalSnake.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Drawing;
 
@@ -15,14 +12,98 @@ namespace Algoritmic
 
         public DiagonalSnake(int size) : base(size) => Direction = Direction.RightTop;
         public DiagonalSnake(int size, Color color) : base(size, color) => Direction = Direction.RightTop;
-        public DiagonalSnake(int size, Color color, Direction direction) : this(size, color) { Direction = direction; }
+        public DiagonalSnake(int size, Color color, Direction direction) : this(size, color) { Direction = direction; }//добавить проверку на диагональныне направление
         public DiagonalSnake(int size, Direction direction) : this(size) { Direction = direction; }
         public DiagonalSnake(int size, Point startPoint) : base(size, startPoint) => Direction = Direction.RightTop;
         public DiagonalSnake(int size, Point startPoint, Direction direction) : this(size, startPoint) { Direction = direction; }
 
         public void Play(int time)
         {
-            throw new NotImplementedException();
+            DateTime dTN = DateTime.Now;
+            while ((DateTime.Now - dTN).TotalSeconds < time)
+            {
+                switch (Direction)
+                {
+                    case Direction.RightTop:
+                        //esli mi dostigaem ugla
+                        if (point.X == 1 && point.Y == 1)
+                            Direction = Direction.Buttom;
+                        else if (point.Y == 1)
+                            Direction = Direction.RightButtom;
+                        else if (point.X == 1)
+                            Direction = Direction.LeftTop;
+                        break;
+                    case Direction.LeftTop:
+                        if (point.X == 0 && point.Y == 1)
+                            Direction = Direction.Right;
+                        else if (point.X == 0)
+                            Direction = Direction.RightTop;
+                        else if (point.Y == 1)
+                            Direction = Direction.LeftButtom;
+                        break;
+                    case Direction.RightButtom:
+                        if (point == new Point(1, 0))
+                            Direction = Direction.Left;
+                        else if (point.X == 1)
+                            Direction = Direction.LeftButtom;
+                        else if (point.Y == 0)
+                            Direction = Direction.RightTop;
+                        break;
+                    case Direction.LeftButtom:
+                        if (point == new Point(0, 0))
+                            Direction = Direction.Top;
+                        else if (point.X == 0)
+                            Direction = Direction.RightButtom;
+                        else if (point.Y == 0)
+                            Direction = Direction.LeftTop;
+                        break;
+                    case Direction.Top:
+                        Direction = Direction.RightTop;
+                        break;
+                    case Direction.Right:
+                        Direction = Direction.RightButtom;
+                        break;
+                    case Direction.Buttom:
+                        Direction = Direction.LeftButtom;
+                        break;
+                    case Direction.Left:
+                        Direction = Direction.LeftTop;
+                        break;
+                }
+                //можно сделать общий метод у родителя для смены позиции
+                switch (Direction)
+                {
+                    case Direction.Buttom:
+                        point.Y--;
+                        break;
+                    case Direction.Left:
+                        point.X--;
+                        break;
+                    case Direction.Top:
+                        point.Y++;
+                        break;
+                    case Direction.Right:
+                        point.X++;
+                        break;
+                    case Direction.RightButtom:
+                        point.X++;
+                        point.Y--;
+                        break;
+                    case Direction.RightTop:
+                        point.X++;
+                        point.Y++;
+                        break;
+                    case Direction.LeftButtom:
+                        point.X--;
+                        point.Y--;
+                        break;
+                    case Direction.LeftTop:
+                        point.X--;
+                        point.Y++;
+                        break;
+                }
+
+            }
         }
 
         public void PlayLooping()
